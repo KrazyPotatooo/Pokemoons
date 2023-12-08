3c8a6e1cbc91180b54e1ccc634061703ec2d333a
KrazyPotatooo <krazymix95@gmail.com>
2023-12-08 22:57:49 +0800

    ggs

--- cardquest/projectsite/cardquest/admin.py
+++ cardquest/projectsite/cardquest/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import PokemonCard
 
-@admin.site.register(PokemonCard)
+admin.site.register(PokemonCard)
+
+
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ("name", "rarity")
     search_fields = ("name",)
+    
