fb83acf2aa8e23aa776e9a59d9b58d7b04e6f154
KrazyPotatooo <krazymix95@gmail.com>
2023-12-10 10:13:31 +0800

    Pokemonimage

--- cardquest/projectsite/cardquest/management/commands/create_initial_data.py
+++ cardquest/projectsite/cardquest/management/commands/create_initial_data.py
@@ -18,12 +18,12 @@ class Command(BaseCommand):
         card2 = PokemonCard(name="Charmeleon", rarity="Rare", hp=120, card_type="Fire", attack="Flamethrower",
                             description="It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.",
                             weakness="Water", card_number=5, release_date="1996-02-27", evolution_stage="Stage 2", abilities="Solar Power",image_url="https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png")
-        card3 = PokemonCard(name="Mewtwo", rarity="Uncommon", hp=150, card_type="Psychic", attack="Psychic",
+        card3 = PokemonCard(name="Mewtwo", rarity="Uncommon", hp=150, card_type="Psychic", attack="Psychic", 
                             description="A genetically engineered pokemon that has powerful psychic abilities.",
-                            weakness="Dark", card_number=10, release_date="1996-02-27", evolution_stage="Basic", abilities="Pressure")
+                            weakness="Dark", card_number=10, release_date="1996-02-27", evolution_stage="Basic", abilities="Pressure",image_url ="https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png")
         card4 = PokemonCard(name="Caterpie", rarity="Common", hp=45, card_type="Grass", attack="Bug Bite",
                             description="For protection, it releases a horrible stench from the antenna on its head to drive away enemies.",
-                            weakness="Fire", card_number=44, release_date="1996-02-27", evolution_stage="Basic", abilities="Shield Dust")
+                            weakness="Fire", card_number=44, release_date="1996-02-27", evolution_stage="Basic", abilities="Shield Dust", image_url = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png")
         card5 = PokemonCard(name="Squirtle", rarity="Rare", hp=40, card_type="Water", attack="Bubble",
                             description="A turtle-like pokemon that can squirt water.",
                             weakness="Grass", card_number=63, release_date="1996-02-27", evolution_stage="Basic", abilities="Torrent")
